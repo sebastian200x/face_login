ac79f36df6cf62bea31c7d77821a3d3d703f43ae
sebastian200x <jetsebastian4@gmail.com>
2024-03-30 09:57:19 +0800

    sql

---
+++ dbhofin.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 30, 2024 at 01:57 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbhofin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_face`
+--
+
+DROP TABLE IF EXISTS `tbl_face`;
+CREATE TABLE IF NOT EXISTS `tbl_face` (
+  `face_id` int NOT NULL AUTO_INCREMENT,
+  `face_img` varchar(255) NOT NULL,
+  PRIMARY KEY (`face_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_property`
+--
+
+DROP TABLE IF EXISTS `tbl_property`;
+CREATE TABLE IF NOT EXISTS `tbl_property` (
+  `property_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `id_no` varchar(255) DEFAULT NULL,
+  `blk_no` int DEFAULT NULL,
+  `lot_no` int DEFAULT NULL,
+  `homelot_area` int DEFAULT NULL,
+  `open_space` int DEFAULT NULL,
+  `sharein_loan` int DEFAULT NULL,
+  `principal_interest` int DEFAULT NULL,
+  `MRI` int DEFAULT NULL,
+  `total` int DEFAULT NULL,
+  PRIMARY KEY (`property_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_property`
+--
+
+INSERT INTO `tbl_property` (`property_id`, `user_id`, `id_no`, `blk_no`, `lot_no`, `homelot_area`, `open_space`, `sharein_loan`, `principal_interest`, `MRI`, `total`) VALUES
+(1, 2, '1', 2, 3, 4, 5, 6, 7, 8, 9),
+(2, 3, '20203', 9, 65, 97569, 57, 65, 756, 87, 6587),
+(3, 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_transaction`
+--
+
+DROP TABLE IF EXISTS `tbl_transaction`;
+CREATE TABLE IF NOT EXISTS `tbl_transaction` (
+  `transac_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `balance_debt` int DEFAULT NULL,
+  `transc_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `due_date` date DEFAULT NULL,
+  `is_verified` varchar(10) NOT NULL DEFAULT 'no',
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `proof` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`transac_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_transaction`
+--
+
+INSERT INTO `tbl_transaction` (`transac_id`, `user_id`, `balance_debt`, `transc_type`, `amount`, `date`, `due_date`, `is_verified`, `code`, `proof`) VALUES
+(1, 2, 3000, 'Cash', NULL, NULL, '2024-03-29', 'no', 'PRE2V', NULL),
+(2, 3, 6, 'arrangement', NULL, NULL, '2024-04-18', 'yes', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_useracc`
+--
+
+DROP TABLE IF EXISTS `tbl_useracc`;
+CREATE TABLE IF NOT EXISTS `tbl_useracc` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `is_admin` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'no',
+  `is_deleted` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'no',
+  `is_verified` varchar(10) NOT NULL DEFAULT 'no',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_useracc`
+--
+
+INSERT INTO `tbl_useracc` (`user_id`, `username`, `password`, `email`, `is_admin`, `is_deleted`, `is_verified`) VALUES
+(1, 'admin', '$2b$12$lTwEqKy98rZhefz/lYFO7OFORY7F/NtPyTnnolKfInFsTojgl2ugO', '', 'yes', 'no', 'yes'),
+(2, '20242', '$2b$12$92vMhEjqOTN/9wM92jsmgeTi9HRSs5Jo5TxYcbvAUzESCR2HyN/C2', 'jetsebastian4@gmail.com', 'no', 'no', 'yes'),
+(3, '20243', '$2b$12$tB9bEdIZlyt08mTJmyI1gOmTYR72q/sA0C9N7nGTvjykBAtG4n8MK', 'agajdbualal@gmail.com', 'no', 'no', 'yes'),
+(4, '20244', '$2b$12$Qm6FwOLLAjh8gSzm0d3/Vu8vpvWZxyD8d8kO8ARjpJtNl2ZKUjBfi', 'asdasd@asdasd.asdasd', 'no', 'no', 'yes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_userinfo`
+--
+
+DROP TABLE IF EXISTS `tbl_userinfo`;
+CREATE TABLE IF NOT EXISTS `tbl_userinfo` (
+  `userinfo_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `given_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `middle_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `last_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `gender` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`userinfo_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tbl_userinfo`
+--
+
+INSERT INTO `tbl_userinfo` (`userinfo_id`, `user_id`, `given_name`, `middle_name`, `last_name`, `gender`) VALUES
+(1, 1, 'Admin', '', '', ''),
+(2, 2, 'Jet', 'Dela cruz', 'Sebastian', 'Male'),
+(3, 3, 'John andrei', 'Samonte', 'Canlas', 'Male'),
+(4, 4, 'A', 'B', 'C', 'Male');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_property`
+--
+ALTER TABLE `tbl_property`
+  ADD CONSTRAINT `tbl_property_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_useracc` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tbl_transaction`
+--
+ALTER TABLE `tbl_transaction`
+  ADD CONSTRAINT `tbl_transaction_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_useracc` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `tbl_userinfo`
+--
+ALTER TABLE `tbl_userinfo`
+  ADD CONSTRAINT `tbl_userinfo_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_useracc` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
